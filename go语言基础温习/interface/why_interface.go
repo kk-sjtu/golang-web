d2329173444808862fb22c0b6166f4436b6324db
Your Name <kkipaper@163.com>
2025-02-06 22:27:30 +0800

    实现了基本的接口,猫叫狗叫

---
+++ go语言基础温习/interface/why_interface.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// 接口是一种类型，是一种抽象的类型
+
+type dog struct{}
+
+func (d dog) say(){
+	fmt.Println("dog say:汪汪汪")
+}
+
+type cat struct{}
+
+func (c cat) say(){
+	fmt.Println("cat say:喵喵喵")
+}
+
+// 定义一个类型，一个抽象的类型。只要实现了say这个方法的类型，
+// 都可以成为sayer类型
+
+type sayer interface{
+	say()
+}
+
+// 接口不管你是什么类型，只管你要实现什么方法
+func da(arg sayer){
+	arg.say()
+}
+
+func main(){
+	c1 := cat{}
+	da(c1)
+	d1 := dog{}
+	da(d1)
+}
