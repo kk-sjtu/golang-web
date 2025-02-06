3f1cca2315335618cbe7a0f453fc04b7866fed9a
Your Name <kkipaper@163.com>
2025-02-06 23:02:19 +0800

    进一步理解接口的意义

--- go语言基础温习/interface/why_interface.go
+++ go语言基础温习/interface/why_interface.go
@@ -5,7 +5,9 @@ import "fmt"
 // 接口是一种类型，是一种抽象的类型
 
 type dog struct{}
-
+type person struct{
+	name string
+}
 func (d dog) say(){
 	fmt.Println("dog say:汪汪汪")
 }
@@ -16,12 +18,17 @@ func (c cat) say(){
 	fmt.Println("cat say:喵喵喵")
 }
 
+func(p person)say(){
+	fmt.Println("person say:啊啊啊")
+}
 // 定义一个类型，一个抽象的类型。只要实现了say这个方法的类型，
 // 都可以成为sayer类型
 
 type sayer interface{
 	say()
 }
+
+
 
 // 接口不管你是什么类型，只管你要实现什么方法
 func da(arg sayer){
@@ -33,4 +40,8 @@ func main(){
 	da(c1)
 	d1 := dog{}
 	da(d1)
+
+	p1 := person{name:"kk"}
+	da(p1)
+
 }
